d37b8d56d0ba271d1926182339d94d9de41d7d99
Cristian Silva <cristianrafas@gamail.com>
2024-02-03 17:08:18 -0300

    Loading

--- script.js
+++ script.js
@@ -56,57 +56,78 @@ async function makeGroups(array) {
 }
 
 async function makePage(page, pagNum) {
+
+    cardRow.innerHTML = "";
+    pagDiv.innerHTML = "";
+
     window.scrollTo({
         top: 0,
         behavior: 'smooth',
     }),
-    cardRow.innerHTML = "";
-    pagDiv.innerHTML = "";
-
-    for (let i = 0; i < groups[page].length; i++) {
-        const characterCard = document.createElement('div');
-        characterCard.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3');
-        let lastEp = (groups[page][i].episode.length) - 1;
-        characterCard.innerHTML = `
+    
+    cardRow.classList.add('loading-indicator');
+    try {
+        for (let i = 0; i < groups[page].length; i++) {
+            const characterCard = document.createElement('div');
+            characterCard.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3');
+            let lastEp = (groups[page][i].episode.length) - 1;
+            characterCard.innerHTML = `
             <div class="row row1">
-                <div class="col-4 p-0 ">
+            <div class="col-4 p-0 ">
                     <img class="card-image" src="${groups[page][i].image}">
-                </div>
-                <div class="col-8 p-0 pb-2 ps-2 ps-md-2">
+                    </div>
+                    <div class="col-8 p-0 pb-2 ps-2 ps-md-2">
                     <div class="card-title">${groups[page][i].name}</div>
                     <p class="card-description"><span class="${groups[page][i].status === 'Dead' ? 'dead status' : groups[page][i].status === 'Alive' ? 'alive status' : 'unknown status'}">O</span> ${groups[page][i].status} - ${groups[page][i].species}</p>
                     <p class="card-description">Última localização conhecida</p>
                     <p class="card-description">${groups[page][i].location.name}</p>
                     <p class="card-description">Visto última vez em</p>
                     <p class="card-description">${await takeLastEp(groups[page][i].episode[lastEp])}</p>
-                </div>
-            </div>
-        `;
-        cardRow.appendChild(characterCard)
-    }
-    const paginationDiv = document.createElement('div');
-    paginationDiv.classList.add('pagination');
+                    </div>
+                    </div>
+                    `;
+                    cardRow.classList.remove('loading-indicator');
+                    cardRow.appendChild(characterCard)
+                }
+        const paginationDiv = document.createElement('div');
+        paginationDiv.classList.add('pagination');
+        
+        if (groups.length > 1) {
+            let prevButtonDisabled = currentPage === 1 ? 'hidden' : '';
+            let prevButtonDisabledTen = currentPage < 11 ? 'hidden' : '';
+            let nextButtonDisabled = currentPage === groups.length ? 'hidden' : '';
+            let nextButtonDisabledTen = currentPage + 10 > groups.length ? 'hidden' : '';
 
-    if (groups.length > 1) {
-        let prevButtonDisabled = currentPage === 1 ? 'hidden' : '';
-        let prevButtonDisabledTen = currentPage < 11 ? 'hidden' : '';
-        let nextButtonDisabled = currentPage === groups.length ? 'hidden' : '';
-        let nextButtonDisabledTen = currentPage + 10 > groups.length ? 'hidden' : '';
-
-        paginationDiv.innerHTML = `
+            paginationDiv.innerHTML = `
     <nav id='pagination-nav' class="d-flex justify-content-center mt-3">
-        <ul class="pagination">
+    <ul class="pagination">
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="prevPage(10)" ${prevButtonDisabledTen}><10</button></li>
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="prevPage(1)" ${prevButtonDisabled}>${(pagNum - 1)}</button></li>
             <li class="page-item"><button class="btn btn-success" disabled>${(pagNum)}</button></li>
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="nextPage(1)" ${nextButtonDisabled}>${(pagNum + 1)}</button></li>
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="nextPage(10)" ${nextButtonDisabledTen}>10></button></li>
-        </ul>
+            </ul>
     </nav>
-`;
-        pagDiv.appendChild(paginationDiv);
+    `;
+            pagDiv.appendChild(paginationDiv);
+        }
+        // hideLoadingIndicator();
     }
+    catch (error) {
+        console.error("Erro ao carregar página:", error);
+        // Lidar com o erro, se necessário
+    } 
+
+    
 }
+
+function showLoadingIndicator() {
+    const loadingIndicator = document.createElement('div');
+    loadingIndicator.classList.add('loading-indicator');
+    console.log('carregando!')
+    cardRow.appendChild(loadingIndicator);
+}
+
 async function takeLastEp(url) {
     try {
         const lastep = await api.get(url);
