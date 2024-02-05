2615394804332c65b93c0aa4c887a776b484fee2
Cristian Silva <cristianrafas@gamail.com>
2024-02-05 17:22:37 -0300

    Modal

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let pageChars = [];
 let groups = [];
 let groupNumber = currentPage - 1;
 let totalPages = 0;
+let searchResults = 1
 
 
 async function takePages(url) {
@@ -50,17 +51,21 @@ async function takeCharsLength() {
 
 async function makeGroups(array) {
     groups = [];
-    for (let i = 0; i < array.length; i += 6) {
-        groups.push(array.slice(i, i + 6));
+    if (array.length) {
+        for (let i = 0; i < array.length; i += 6) {
+            groups.push(array.slice(i, i + 6));
+        }
+        return groups;
     }
-    return groups;
+    else {
+        searchResults = 0;
+    }
 }
 
 function loading() {
-    // loadingDiv.innerHTML = "";
     for (let i = 1; i <= 6; i++) {
         const loader = document.createElement(`div`);
-        loader.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3', 'z-0');
+        loader.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3');
         loader.id = `loadingDiv-${i}`;
         loader.innerHTML = `
         <div class="row pt-1 pt-md-0 row1">
@@ -76,8 +81,8 @@ function loading() {
 }
 
 async function makePage(page, pagNum) {
-    cardRow.innerHTML="";
-    pagDiv.innerHTML="";
+    cardRow.innerHTML = "";
+    pagDiv.innerHTML = "";
 
     loading()
     window.scrollTo({
@@ -88,11 +93,11 @@ async function makePage(page, pagNum) {
     try {
         for (let i = 0; i < groups[page].length; i++) {
             const characterCard = document.createElement('div');
-            characterCard.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3', 'z-2');
+            characterCard.classList.add('col-11', 'col-md-5', 'card-div', 'mb-3');
             let lastEp = (groups[page][i].episode.length) - 1;
             characterCard.innerHTML = `
-            <div class="row row1">
-            <div class="col-4 p-0 ">
+                    <div class="row row1">
+                    <div class="col-4 p-0 ">
                     <img class="card-image" src="${groups[page][i].image}">
                     </div>
                     <div class="col-8 p-0 pb-2 ps-3 ps-md-2">
@@ -105,6 +110,7 @@ async function makePage(page, pagNum) {
                     </div>
                     </div>
                     `;
+            characterCard.addEventListener('click', () => { openModal(groups[page][i]);})
             cardRow.appendChild(characterCard)
         }
         for (let i = 1; i <= 6; i++) {
@@ -125,7 +131,7 @@ async function makePage(page, pagNum) {
             paginationDiv.innerHTML = `
     <nav id='pagination-nav' class="d-flex justify-content-center mt-3">
     <ul class="pagination">
-            <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="prevPage(10)" ${prevButtonDisabledTen}><10</button></li>
+    <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="prevPage(10)" ${prevButtonDisabledTen}><10</button></li>
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="prevPage(1)" ${prevButtonDisabled}>${(pagNum - 1)}</button></li>
             <li class="page-item"><button class="btn btn-success" disabled>${(pagNum)}</button></li>
             <li class="page-item"><button class="btn btn-secondary opacity-50" onclick="nextPage(1)" ${nextButtonDisabled}>${(pagNum + 1)}</button></li>
@@ -135,16 +141,58 @@ async function makePage(page, pagNum) {
     `;
             pagDiv.appendChild(paginationDiv);
         }
-        // hideLoadingIndicator();
     }
     catch (error) {
         console.error("Erro ao carregar página:", error);
-        // Lidar com o erro, se necessário
     }
-
-
 }
 
+async function openModal(char) {
+    try {
+        const modalContent = document.getElementById('modalContent');
+        const modal = document.getElementById('myModal');
+        const modalContentWrapper = document.createElement('div');
+        modalContentWrapper.classList.add('d-fluid', 'justify-content-center', 'align-items-center');
+        const characterCardModal = document.createElement('div');
+        characterCardModal.classList.add('col-12', 'col-md-11', 'card-modal');
+        let lastEp = (char.episode.length) - 1;
+        characterCardModal.innerHTML = `
+        <div class="row-10 row-md-12 row-modal d-flex">
+                <div class="col-12 col-md-4 p-0 image-modal-col">
+                <img class="card-image-modal img-fluid ps-mb-4 pb-mb-4" src="${char.image}">
+                </div>
+                <div class="col-12 col-md-8 ps-md-5 text-col-modal">
+                <div class="card-title-modal">${char.name}</div>
+                <p class="card-description-modal ps-2"><span class="${char.status === 'Dead' ? 'dead status' : char.status === 'Alive' ? 'alive status' : 'unknown status'}">O</span> ${char.status} - ${char.species}</p>
+                <p class="card-description-modal ps-2">Última localização conhecida</p>
+                <p class="card-description-modal ps-2">${char.location.name}</p>
+                <p class="card-description-modal ps-2">Visto última vez em</p>
+                <p class="card-description-modal ps-2">${await takeLastEp(char.episode[lastEp])}</p>
+                </div>
+                </div>
+                `;
+                
+                modalContentWrapper.appendChild(characterCardModal);
+        modalContent.appendChild(modalContentWrapper);
+        
+        console.log(char.name);
+        modal.style.display = 'block';
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+function closeModal() {
+    const modal = document.getElementById('myModal');
+    const modalContent = document.getElementById('modalContent');
+    
+    modalContent.innerHTML = '';
+    
+    modal.style.display = 'none';
+}
 
 async function takeLastEp(url) {
     try {
@@ -155,6 +203,7 @@ async function takeLastEp(url) {
         console.log('Erro ao carregar dados.', error);
     }
 }
+
 function nextPage(num) {
     if (currentPage > totalPages) {
         currentPage += num;
@@ -206,10 +255,14 @@ document.getElementById('search').addEventListener('keypress', function (event) 
 async function search(word) {
     try {
         const searchChars = allChars.filter(character => character.name.toLowerCase().includes(word));
-        console.log(searchChars);
         await makeGroups(searchChars);
         currentPage = 1;
-        await makePage(groupNumber, currentPage);
+        if (searchResults) {
+            await makePage(groupNumber, currentPage);
+        }
+        else {
+            alert('A pesquisa não encontrou resultados!')
+        }
     } catch (error) {
         console.log('Erro ao fazer pesquisa', error);
     }
